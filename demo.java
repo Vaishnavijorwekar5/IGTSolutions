3f4688823eef28550bb4e2a8ccf853cb66aecfe4
Vaishnavi Sanjay Jorwekar <Vaishnavi.Jorwekar@igtsolutions.com>
2023-06-22 09:59:42 +0530

    first commit

---
+++ demo.java
@@ -0,0 +1,12 @@
+// click on the folder type cmd then automatically open cmd for that folder
+
+class Demo1
+{
+
+ 	public static void main(String arg[])
+	{
+		
+		System.out.println(" Hii Welcome to JAVA !!");
+
+	}
+}
